2bcb841fcc08de787e455f94afb7d690412e0fca
mauroj4ckunas <96546837+mauroj4ckunas@users.noreply.github.com>
2023-05-31 21:35:49 -0300

    Fondo en el chat

--- hasbulla-chat/src/components/Wordspace.tsx
+++ hasbulla-chat/src/components/Wordspace.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import '../index.css';
 import ListaChats from './ListaChats';
+import fondo from '../assets/fondo-hasbulla.png'
 
 export default function WordspaceComp() {
   return (
     <>
     <div className='flex'>
-      <section id="lista-chats" className='bg-gray-800 h-screen lg:w-1/4 hidden lg:block'>
+      <section id="lista-chats" className='bg-gray-800 h-screen lg:w-1/4'>
         <ListaChats/>
       </section>
-      <section id="chat-abierto" className="bg-gray-900 lg:w-3/4 w-full">
+      <section id="chat-abierto" className="bg-gray-900 lg:w-3/4 w-full" style={{ backgroundImage: `url(${fondo})` }}>
       </section>
     </div>
     </>
