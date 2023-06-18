c18ee4768426f46b7f998ae6c2ea284d98a44ea5
mauroj4ckunas <96546837+mauroj4ckunas@users.noreply.github.com>
2023-06-18 20:05:40 -0300

    a

--- hasbulla-chat/src/components/ChatAbierto.tsx
+++ hasbulla-chat/src/components/ChatAbierto.tsx
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import NombreDeChat from './NombreDeChat';
-import InputMensaje from './InputMensaje';
 import RepositorioMensajes from './RepositorioMensajes';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShare } from '@fortawesome/free-solid-svg-icons';
+import {db} from '../firebase'
+import { addDoc, collection } from 'firebase/firestore/lite';
 
 export default function ChatAbierto(){
+
+    // const [mensaje,setMensaje] = useState("Que pelota Gerson")
+
+    // const enviarMensaje = () => {
+    //     console.log(mensaje);
+    //     setMensaje("");
+    // }
+
+    const addMensaje = async () => {
+        const mensajesCol = collection(db, 'mensajeTest');
+        await addDoc(mensajesCol, {
+            mensaje: 'Que pelota Gerson',
+        });
+    }
+
+
     return(
         <div className='h-screen w-full relative flex flex-col'>
             <NombreDeChat/>
-            <RepositorioMensajes/>
-            <InputMensaje/>
+            <RepositorioMensajes subirMensaje={addMensaje}/>
+            <div className="mt-auto flex w-full justify-center">  
+                <input id="input_mensaje" type="text" placeholder="Escriba un mensaje"
+                    className="my-5 px-4 py-3 w-2/3 rounded-full
+                    bg-gray-900 text-white"
+                    value={'Que pelota Gerson'}
+                >
+                </input>
+                <button id="enviar" className="my-5 ml-2 px-4 rounded-full
+                        bg-cyan-300 hover:bg-cyan-50" 
+                        onClick={addMensaje}
+                    >
+                    <FontAwesomeIcon icon={faShare} size="2x"/>
+                </button>
+            </div>
         </div>
     );
 }
--- hasbulla-chat/src/components/RepositorioMensajes.tsx
+++ hasbulla-chat/src/components/RepositorioMensajes.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import { MensajeEnviado, MensajeRecibido } from './Mensaje';
 
-export default function RepositorioMensajes(){
+type Mensaje = {
+    texto: string,
+    fechaDeEnvio: Date,
+    origen: boolean,
+}
+
+export default function RepositorioMensajes(props: any){
+    
+    
+
     return (
         <div className='w-full h-full flex justify-center overflow-y-auto scroll_chat'>
             <div className='my-2 w-3/4 flex flex-col space-y-2 text-black text-left'>
                 <MensajeEnviado/>
-                <MensajeRecibido/>
-                <MensajeEnviado/>
-                <MensajeRecibido/>
-                <MensajeEnviado/>
-                <MensajeRecibido/>
-                <MensajeEnviado/>
-                <MensajeRecibido/>
-                <MensajeEnviado/>
-                <MensajeRecibido/>
-                <MensajeEnviado/>
-                <MensajeRecibido/>
             </div>
         </div>
     );
