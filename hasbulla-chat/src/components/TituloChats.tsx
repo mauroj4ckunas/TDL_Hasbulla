b09b0196869038048b2c400180867726628526e5
mauroj4ckunas <96546837+mauroj4ckunas@users.noreply.github.com>
2023-07-04 22:55:21 -0300

    arreglado error de responsive de agregar/cerrar sesion

--- hasbulla-chat/src/components/TituloChats.tsx
+++ hasbulla-chat/src/components/TituloChats.tsx
@@ -16,20 +16,27 @@ export default function TituloChats({modalAgregarUsuario, modalCierreSesion}: Pr
     return (
         <>  
         <div className="flex justify-between mb-2">
-            <div className="text-white text-2xl p-4">
+            <div className="text-white text-2xl p-4 mt-2">
                 <h2 className="italic">Chats</h2>
             </div>
-            <div>
-                <SpeedDial ariaLabel="Opciones de mensajes" sx={{position: 'absolute', bottom: 522, left: 240}} icon={<FontAwesomeIcon icon={faPlus} size="2xl"/>} direction="down">   
+            <div className="hidden md:block" style={{ position: 'absolute', left: '15%', top: '0.01%', marginRight: '5px' }}>
+                <SpeedDial
+                    ariaLabel="Opciones de mensajes"
+                    sx={{ position: 'relative', mt: 2, mr: 2, zIndex: 20 }}
+                    icon={<FontAwesomeIcon icon={faPlus} size="2xl" />}
+                    direction="down"
+                >   
                     <SpeedDialAction
                         key={'Usuarios'}
-                        icon={<FontAwesomeIcon icon={faUser} size="lg"/>}
+                        sx={{zIndex: 50}}
+                        icon={<FontAwesomeIcon icon={faUser} size="lg" />}
                         tooltipTitle={'Agregar Contacto'}
                         onClick={modalAgregarUsuario}
                     />
                     <SpeedDialAction
                         key={'Sesion'}
-                        icon={<FontAwesomeIcon icon={faRightFromBracket} size="lg"/>}
+                        sx={{zIndex: 50}}
+                        icon={<FontAwesomeIcon icon={faRightFromBracket} size="lg" />}
                         tooltipTitle={'Cerrar Sesión'}
                         onClick={modalCierreSesion}
                     />
