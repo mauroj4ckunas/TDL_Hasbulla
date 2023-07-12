b127904a91166f5f2067f6c312df4274e2efb8ac
mauroj4ckunas <96546837+mauroj4ckunas@users.noreply.github.com>
2023-07-11 23:40:36 -0300

    Eliminamos el back de Express

--- hasbulla-back/routers/loginRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { postLogin, getLogin } from '../controllers/loginController';
-
-const router = express.Router();
-
-router.post('/', postLogin);
-router.get('/', getLogin);
-
-export default router;
