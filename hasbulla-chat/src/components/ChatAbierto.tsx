334c6663d1b492d2431906ba19165653b5765240
mauroj4ckunas <96546837+mauroj4ckunas@users.noreply.github.com>
2023-07-14 17:30:22 -0300

    correccion mensajes repetidos

--- hasbulla-chat/src/components/ChatAbierto.tsx
+++ hasbulla-chat/src/components/ChatAbierto.tsx
@@ -21,7 +21,7 @@ interface Props {
     bd: BD,
 }
 
-export default function ChatAbierto({chat, usuarioLogueado, contacto, bd}: Props){
+export default function ChatAbierto({ chat, usuarioLogueado, contacto, bd }: Props) {
 
     const [mensajesMostrados, setMensajesMostrados] = useState<Mensajes[]>([]);
     const [idMensajes, setIdMensajes] = useState<number>(0);
@@ -55,10 +55,10 @@ export default function ChatAbierto({chat, usuarioLogueado, contacto, bd}: Props
 
     const escucharMensajes = async (idChat: number) => {
         await onSnapshot(collection(doc(bd.getBD(), "Chats", idChat.toString()), "Mensajes"), (querySnapshot) => {
-            let response = querySnapshot.docs.sort((a, b) => parseInt(a.data().idMensaje) - parseInt(b.data().idMensaje))[querySnapshot.docs.length-1].data()
-            setIdMensajes(idMensajes+1);
+            let response = querySnapshot.docs.sort((a, b) => parseInt(a.data().idMensaje) - parseInt(b.data().idMensaje))[querySnapshot.docs.length - 1].data()
+            setIdMensajes(idMensajes + 1);
             setMensajeRecibido({
-                idMensaje: response.idMensaje+1,
+                idMensaje: response.idMensaje + 1,
                 texto: response.texto,
                 usuarioEmisor: response.usuarioEmisor,
                 usuarioReceptor: response.usuarioReceptor,
@@ -77,7 +77,7 @@ export default function ChatAbierto({chat, usuarioLogueado, contacto, bd}: Props
     const inputTextRef = useRef<HTMLInputElement | null>(null);
     const divRepoMensajesRef = useRef<HTMLDivElement | null>(null);
 
-    const getFechaActual = () =>{
+    const getFechaActual = () => {
         const e = new Date();
         return e.toLocaleDateString();
     }
@@ -120,7 +120,8 @@ export default function ChatAbierto({chat, usuarioLogueado, contacto, bd}: Props
     };
 
     const agregarComponenteMensajeRecibido = () => {
-        if(mensajeRecibido){
+        console.log(mensajeRecibido && mensaje.idMensaje !== mensajeRecibido.idMensaje)
+        if (mensajeRecibido) {
             setMensajesMostrados([...mensajesMostrados, mensajeRecibido])
         }
     };
@@ -133,9 +134,9 @@ export default function ChatAbierto({chat, usuarioLogueado, contacto, bd}: Props
                 fechaDeEnvio: getFechaActual(),
                 imagen: base64,
                 coordenadas: coordenadas,
-                idMensaje: idMensajes+1,
+                idMensaje: idMensajes + 1,
             }));
-            setIdMensajes(idMensajes+1)
+            setIdMensajes(idMensajes + 1)
         }
         if (inputTextRef.current != null) {
             inputTextRef.current.value = "";
@@ -168,7 +169,7 @@ export default function ChatAbierto({chat, usuarioLogueado, contacto, bd}: Props
         setModalUbicacion(false);
         setCoordenadas([]);
     };
-    
+
     const enviarUbicacion = (coordenadas: number[]) => {
         handlerCerrarModalUbicacion();
         setCoordenadas(coordenadas);
@@ -180,34 +181,34 @@ export default function ChatAbierto({chat, usuarioLogueado, contacto, bd}: Props
         setCoordenadas([]);
     }
 
-    return(
+    return (
         <div className='h-screen w-full relative flex flex-col'>
-            <NombreDeChat contacto={contacto.nombre}/>
+            <NombreDeChat contacto={contacto.nombre} />
             <div ref={divRepoMensajesRef} className='w-full h-full flex justify-center overflow-y-auto scroll_chat'>
                 <div className='my-2 w-3/5 flex flex-col space-y-2 text-black text-left' >
                     {mensajesMostrados.map((msj, index) => {
-                        return msj.usuarioEmisor === usuarioLogueado.username ? <MensajeEnviado key={index} mensaje={msj}/> : <MensajeRecibido key={index} mensaje={msj}/>
+                        return msj.usuarioEmisor === usuarioLogueado.username ? <MensajeEnviado key={index} mensaje={msj} /> : <MensajeRecibido key={index} mensaje={msj} />
                     })}
                 </div>
             </div>
             <div className="mt-auto w-full flex justify-center">
                 <div className='w-1/4 mb-5 flex justify-center'>
-                    <SpeedDial ariaLabel="Opciones de mensajes" sx={{ position: 'absolute', top: 460}} icon={<FontAwesomeIcon icon={faBars} size="lg"/>}>   
+                    <SpeedDial ariaLabel="Opciones de mensajes" sx={{ position: 'absolute', top: 460 }} icon={<FontAwesomeIcon icon={faBars} size="lg" />}>
                         <SpeedDialAction
                             key={'Cancelar'}
-                            icon={<FontAwesomeIcon icon={faXmark} size="lg"/>}
+                            icon={<FontAwesomeIcon icon={faXmark} size="lg" />}
                             tooltipTitle={'Cancelar elección'}
                             onClick={handlerCancelarEleccion}
                         />
                         <SpeedDialAction
                             key={'Imágen'}
-                            icon={<FontAwesomeIcon icon={faImage} size="lg"/>}
+                            icon={<FontAwesomeIcon icon={faImage} size="lg" />}
                             tooltipTitle={'Enviar Imágen'}
                             onClick={handlerAbrirModalImagen}
                         />
                         <SpeedDialAction
                             key={'Ubicación'}
-                            icon={<FontAwesomeIcon icon={faLocationDot} size="lg"/>}
+                            icon={<FontAwesomeIcon icon={faLocationDot} size="lg" />}
                             tooltipTitle={'Enviar Ubicación'}
                             onClick={handlerAbrirModalUbicacion}
                         />
@@ -215,12 +216,12 @@ export default function ChatAbierto({chat, usuarioLogueado, contacto, bd}: Props
                 </div>
                 <div className='w-3/4 flex'>
                     {base64 !== '' && <div className='absolute flex w-1/5 h-1/4 bg-cyan-900 p-2 top-[64%] right-[30%] rounded-md items-center justify-center z-10'>
-                        <img src={base64} alt={'Imágen cargada'} className='max-w-full max-h-full'/>
+                        <img src={base64} alt={'Imágen cargada'} className='max-w-full max-h-full' />
                     </div>}
                     {coordenadas.length !== 0 && <div className='absolute flex w-1/5 h-1/4 bg-cyan-900 p-2 top-[64%] right-[30%] rounded-md items-center justify-center z-10'>
-                        <MapaUbicacionActual coordenadas={[coordenadas[0], coordenadas[1]]}/>
+                        <MapaUbicacionActual coordenadas={[coordenadas[0], coordenadas[1]]} />
                     </div>}
-                    <input ref={ inputTextRef }
+                    <input ref={inputTextRef}
                         id="input_mensaje" type="text" placeholder="Escriba un mensaje"
                         className="my-5 px-4 py-3 w-2/3 rounded-full
                         bg-gray-900 text-white"
@@ -229,14 +230,14 @@ export default function ChatAbierto({chat, usuarioLogueado, contacto, bd}: Props
                     </input>
                     <button id="enviar" className="my-5 ml-2 px-4 rounded-full
                             bg-cyan-300 hover:bg-cyan-50"
-                            onClick={enviarMensaje}
-                        >
-                        <FontAwesomeIcon icon={faShare} size="2xl"/>
+                        onClick={enviarMensaje}
+                    >
+                        <FontAwesomeIcon icon={faShare} size="2xl" />
                     </button>
                 </div>
             </div>
-            {modalImagen && <EnviarImagen show={modalImagen} cerrarModal={handlerCerrarModalImagen} enviar={enviarImagen}/>}
-            {modalUbicacion && <EnviarUbicacion show={modalUbicacion} cerrarModal={handlerCerrarModalUbicacion} enviar={enviarUbicacion}/>}
+            {modalImagen && <EnviarImagen show={modalImagen} cerrarModal={handlerCerrarModalImagen} enviar={enviarImagen} />}
+            {modalUbicacion && <EnviarUbicacion show={modalUbicacion} cerrarModal={handlerCerrarModalUbicacion} enviar={enviarUbicacion} />}
         </div>
     );
 }
