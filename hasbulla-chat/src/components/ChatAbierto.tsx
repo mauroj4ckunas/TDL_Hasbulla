37162ba6f663857689c78399d0af28983f67b863
mauroj4ckunas <96546837+mauroj4ckunas@users.noreply.github.com>
2023-07-14 17:51:58 -0300

    cambio: se compara los uaurios y no la id de mensajes

--- hasbulla-chat/src/components/ChatAbierto.tsx
+++ hasbulla-chat/src/components/ChatAbierto.tsx
@@ -56,9 +56,7 @@ export default function ChatAbierto({ chat, usuarioLogueado, contacto, bd }: Pro
     const escucharMensajes = async (idChat: number) => {
         await onSnapshot(collection(doc(bd.getBD(), "Chats", idChat.toString()), "Mensajes"), (querySnapshot) => {
             let response = querySnapshot.docs.sort((a, b) => parseInt(a.data().idMensaje) - parseInt(b.data().idMensaje))[querySnapshot.docs.length - 1].data()
-            console.log("id response: ", response.idMensaje)
-            console.log("id mensaje: ", mensaje.idMensaje)
-            if (!(response.idMensaje !== mensaje.idMensaje)) {
+            if (response.usuarioEmisor !== usuarioLogueado.username) {
                 setIdMensajes(idMensajes + 1);
                 setMensajeRecibido({
                     idMensaje: response.idMensaje + 1,
@@ -120,12 +118,10 @@ export default function ChatAbierto({ chat, usuarioLogueado, contacto, bd }: Pro
     }, [mensajesMostrados]);
 
     const agregarComponenteMensaje = () => {
-        console.log("Entra en agregarComponenteMensaje")
         setMensajesMostrados([...mensajesMostrados, mensaje])
     };
 
     const agregarComponenteMensajeRecibido = () => {
-        console.log("Entra en agregarComponenteMensajeRecibido")
         if (mensajeRecibido) {
             setMensajesMostrados([...mensajesMostrados, mensajeRecibido])
             setMensajeRecibido(undefined);
