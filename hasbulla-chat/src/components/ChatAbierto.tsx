776dd6a6f3d639095dd63c4ef615e04eab5537c4
mauroj4ckunas <96546837+mauroj4ckunas@users.noreply.github.com>
2023-07-16 21:24:03 -0300

    pusheo ultima version?

--- hasbulla-chat/src/components/ChatAbierto.tsx
+++ hasbulla-chat/src/components/ChatAbierto.tsx
@@ -111,6 +111,7 @@ export default function ChatAbierto({ chat, usuarioLogueado, contacto, bd }: Pro
     }, [mensaje]);
 
     useEffect(() => {
+        setMensajeRecibido(mensajeRecibido);
         agregarComponenteMensajeRecibido();
     }, [mensajeRecibido]);
 
